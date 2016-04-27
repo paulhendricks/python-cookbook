b335279c320c2e5080068fc4cd64457ac362a461
Paul Hendricks <paul.hendricks.2013@owu.edu>
2016-04-27 10:56:12 -0400

    added a chapter on deques...keeping a limited history

--- ch01/1.3.py
+++ ch01/1.3.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python3
 """Keeping the last N items
 """
+from collections import deque
 
-p = range(10)
-x, y, *z = p
+
+def search(lines, pattern, history=5):
+    previous_lines = deque(maxlen=history)
+    for line in lines:
+        if pattern in line:
+            yield line, previous_lines
+        previous_lines.append(line)
+
+
+if __name__ == "__main__":
+    with open('somefile.txt') as f:
+        for l, prevlines in search(f, 'python'):
+            for pline in prevlines:
+                print(pline, end='')
+            print(l, end='')
+            print('-' * 20)
