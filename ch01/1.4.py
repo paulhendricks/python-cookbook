5b7ff9758e74ce5f620c4ae840f61199dce73509
Paul Hendricks <paul.hendricks.2013@owu.edu>
2016-05-01 12:00:00 -0500

    modified some examples

--- ch01/1.4.py
+++ ch01/1.4.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
-"""Keeping the last N items
-"""
+"""Finding the Largest or Smallest N Items
 
-p = range(10)
-x, y, *z = p
+Complete!
+"""
+import heapq
+
+
+if __name__ == '__main__':
+    nums = [3, 7, 4, 2, 1, 94, 48, 34]
+    print(heapq.nlargest(3, nums))
+    print(heapq.nsmallest(3, nums))
