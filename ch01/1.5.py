35c19ed8240fcaadd3a7673fd9e62b8a76f45539
Paul Hendricks <paul.hendricks.2013@owu.edu>
2016-05-02 12:00:00 -0500

    modified some examples

--- ch01/1.5.py
+++ ch01/1.5.py
@@ -1,6 +1,39 @@
 #!/usr/bin/python3
-"""Keeping the last N items
-"""
+"""Implementing a Priority Queue
 
-p = range(10)
-x, y, *z = p
+Complete!
+"""
+import heapq
+
+
+class PriorityQueue:
+    def __init__(self):
+        self._queue = []
+        self._index = 0
+
+    def push(self, item, priority):
+        heapq.heappush(self._queue, (-priority, self._index, item))
+        self._index += 1
+
+    def pop(self):
+        return heapq.heappop(self._queue)[-1]
+
+
+class Item:
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return 'Item({!r})'.format(self.name)
+
+
+if __name__ == '__main__':
+    q = PriorityQueue()
+    q.push(Item('foo'), 1)
+    q.push(Item('bar'), 5)
+    q.push(Item('spam'), 4)
+    q.push(Item('grok'), 1)
+    print(q.pop())
+    print(q.pop())
+    print(q.pop())
+    print(q.pop())
