48577767966a228acdfdc0e0a35e5b137b687721
Paul Hendricks <paul.hendricks.2013@owu.edu>
2016-05-03 11:27:33 -0400

    added more examples

---
+++ ch01/1.11.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Naming a Slice
+
+Complete!
+"""
+
+
+if __name__ == '__main__':
+    items = [1, 5, 2, 9, 10, 1, 3, 6, 5]
+    a = slice(4, 6)
+    print(items)
+    print(a)
+    print(items[a])
