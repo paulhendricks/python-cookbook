9102a7bd3c7f249c3d2d3a2ebb24afb7f2569036
Paul Hendricks <paul.hendricks.2013@owu.edu>
2016-05-04 16:44:02 -0400

    added final example

--- ch01/1.20.py
+++ ch01/1.20.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
-"""TO BE EDITED
+"""Combining Multiple Mappings into a Single Mapping
 
-Not Complete!
+Complete!
 """
+from collections import ChainMap
 
 
 if __name__ == '__main__':
-    print("Hello world!")
+    a = {'x': 1, 'z': 3}
+    b = {'y': 2, 'z': 4}
+
+    # (a) Simple example of combining
+    c = ChainMap(a, b)
+    print(c['x'])  # Outputs 1  (from a)
+    print(c['y'])  # Outputs 2  (from b)
+    print(c['z'])  # Outputs 3  (from a)
