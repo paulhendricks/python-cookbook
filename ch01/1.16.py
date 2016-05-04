53c32ef5ccd91e3d06d01339e0287f23a2bd2216
Paul Hendricks <paul.hendricks.2013@owu.edu>
2016-05-04 16:28:42 -0400

    added example

--- ch01/1.16.py
+++ ch01/1.16.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-"""TO BE EDITED
+"""Filtering Sequence Elements
 
 Not Complete!
 """
 
 
 if __name__ == '__main__':
-    print("Hello world!")
+    items = [1, 2, 3, 4, 5, 6]
+    filtered = [i for i in items if i < 3]
+    print(filtered)
+    filtered = [i if i < 3 else None for i in items]
+    print(filtered)
