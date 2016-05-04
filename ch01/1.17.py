4209137e5fcb65a0a1d2c5ee386713b11f1f1cdf
Paul Hendricks <paul.hendricks.2013@owu.edu>
2016-05-04 16:32:41 -0400

    added example

--- ch01/1.17.py
+++ ch01/1.17.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
-"""TO BE EDITED
+"""Extracting a Subset of a Dictionary
 
-Not Complete!
+Complete!
 """
 
 
 if __name__ == '__main__':
-    print("Hello world!")
+    prices = {
+        'ACME': 45.23,
+        'AAPL': 612.78,
+        'IBM': 205.55,
+        'HPQ': 37.20,
+        'FB': 10.75
+    }
+
+    # Make a dictionary of all prices over 200
+    p1 = {key: value for key, value in prices.items() if value > 200}
+    print(p1)
