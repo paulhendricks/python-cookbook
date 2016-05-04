dfb9f4e82dbf06fb4d4c6b3ac2ec72f0e1f8505b
Paul Hendricks <paul.hendricks.2013@owu.edu>
2016-05-04 16:38:34 -0400

    added another example

--- ch01/1.19.py
+++ ch01/1.19.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
-"""TO BE EDITED
+"""Transforming and Reducing Data at the Same Time
 
-Not Complete!
+Complete!
 """
 
 
 if __name__ == '__main__':
-    print("Hello world!")
+    # Output a tuple as CSV
+    s = ('ACME', 50, 123.45)
+    print(','.join(str(x) for x in s))
+
+    # Data reduction across fields of a data structure
+    portfolio = [
+        {'name': 'GOOG', 'shares': 50},
+        {'name': 'YHOO', 'shares': 75},
+        {'name': 'AOL', 'shares': 20},
+        {'name': 'SCOX', 'shares': 65}
+    ]
+    min_shares = min(s['shares'] for s in portfolio)
+    print(min_shares)
