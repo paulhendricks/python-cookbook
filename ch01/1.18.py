2fec540e7685707f526be7a0c2036ae65d1d2cde
Paul Hendricks <paul.hendricks.2013@owu.edu>
2016-05-04 16:36:02 -0400

    added another example

--- ch01/1.18.py
+++ ch01/1.18.py
@@ -1,9 +1,25 @@
 #!/usr/bin/python3
-"""TO BE EDITED
+"""Mapping Names to Sequence Elements
 
-Not Complete!
+Complete!
 """
+from collections import namedtuple
+
+
+Stock = namedtuple('Stock', ['name', 'shares', 'price'])
+
+
+def compute_cost(recs):
+    total = 0.0
+    for rec in recs:
+        s = Stock(*rec)
+        total += s.shares * s.price
+    return total
 
 
 if __name__ == '__main__':
-    print("Hello world!")
+    # Some Data
+    records = [('GOOG', 100, 490.1),
+               ('ACME', 100, 123.45),
+               ('IBM', 50, 91.15)]
+    print(compute_cost(records))
