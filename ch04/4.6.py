78fdbb2167760b0d13e687777eb15d73512b2ad6
Paul Hendricks <paul.hendricks.2013@owu.edu>
2016-05-18 11:36:50 -0400

    added example

--- ch04/4.6.py
+++ ch04/4.6.py
@@ -3,7 +3,26 @@
 
 Not Complete!
 """
+from collections import deque
+
+class linehistory:
+    def __init__(self, lines, histlen=3):
+        self.lines = lines
+        self.history = deque(maxlen=histlen)
+
+    def __iter__(self):
+        for lineno, line in enumerate(self.lines,1):
+            self.history.append((lineno, line))
+            yield line
+
+    def clear(self):
+        self.history.clear()
 
 
 if __name__ == '__main__':
-    print("Hello world!")
+    with open('somefile.txt') as f:
+        lines = linehistory(f)
+        for line in lines:
+            if 'python' in line:
+                for lineno, hline in lines.history:
+                    print('{}:{}'.format(lineno, hline), end='')
