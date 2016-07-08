fea9a3aee166d2c71c80166b46c85a3ecdd12dbe
Paul Hendricks <paul.hendricks.2013@owu.edu>
2016-07-08 11:04:00 -0500

    added example

--- ch04/4.15.py
+++ ch04/4.15.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-"""TO BE EDITED
+"""Iterating in Sorted Order Over Merged Sorted Iterables
 
-Not Complete!
+Complete!
 """
+import heapq
 
 
 if __name__ == '__main__':
-    print("Hello world!")
+    a = [1, 4, 7, 10]
+    b = [2, 5, 6, 11]
+    for c in heapq.merge(a, b):
+        print(c)
